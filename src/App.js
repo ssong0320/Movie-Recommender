cfef8283e8069e73afadae6d35c3ddb3b2f2d6d3
ssong0320 <118571777+ssong0320@users.noreply.github.com>
2024-01-02 14:59:31 -0500

    site structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [movie, setMovie] = useState('');
+
+  const handleInputChange = (event) => {
+    setMovie(event.target.value);
+  }
+
   return (
-    <div className="App">
-    <h1>movie rec project</h1>
+    <div style={{ textAlign: 'center' }}>
+      <h1>Movie Recommendation Project</h1>
+        <input
+            type="text"
+            placeholder="Enter a movie name..."
+            value={movie}
+            onChange={handleInputChange}
+            style={{ width: '700px', height: '60px', fontSize: '40px', marginTop: '100px'  }}
+          />
+        <div style={{ marginTop: '100px'}}>
+          <p style={{ fontSize: "20px" }}>Recommended Movies: {movie}</p>
+          <ul style={{ display: 'flex', listStyleType: 'none', padding: '0', marginTop: '70px', width: '100%' }}>
+            <li style={{ flexGrow: 1, textAlign: 'center' }}>movie 1</li>
+            <li style={{ flexGrow: 1, textAlign: 'center' }}>movie 2</li>
+            <li style={{ flexGrow: 1, textAlign: 'center' }}>movie 3</li>
+            <li style={{ flexGrow: 1, textAlign: 'center' }}>movie 4</li>
+            <li style={{ flexGrow: 1, textAlign: 'center' }}>movie 5</li>
+          </ul>
+        </div>
     </div>
   );
 }
